37f15c10b916ece5cd15ddf6b64313956c2634d9
Wing Yin Chan <66709835+wingchan852@users.noreply.github.com>
2023-05-14 20:49:41 -0700

    fixed typo

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,7 +52,7 @@ function initializeServiceWorker() {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
-      const registration = navigator.serviceWorker.register('./sw.js')
+      const registration = navigator.serviceWorker.register('./sw.js');
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
       if(registration){
